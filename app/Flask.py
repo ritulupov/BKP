9515701196f6787808ae0e16071a171775b6fa20
ritulupov <r.i.tulupov@gmail.com>
2023-04-22 08:46:03 +0300

    Приложение работает

--- app/Flask.py
+++ app/Flask.py
@@ -1,80 +1,57 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# # Приложение для демонстрации
-
-# **Примечание:** Flask-приложения, которые необходимо разворачивать на сервере, создаются в виде файлов с расширением **.py**
 # 
-# В данном уроке используется формат Jupyter Notebook исключительно для удобства *демонстрации*
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 from flask import Flask, render_template, request
-# псевдокод:
-# из файла my_module.py импортируем функции
-# from my_module import func1, func2, ...
-# Например: model.py, preprocessing.py, visualisation.py | postprocessing.py
+from joblib import load
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, Normalizer
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+import numpy as np
+
+# Загрузить модель с диска
+# Перенести процесс предобработки
 
 app = Flask(__name__)
 
+['Модуль упругости при растяжении, ГПа','Прочность при растяжении, МПа',
+             'Соотношение матрица-наполнитель'
+             ]
 
-# псевдокод
-# def get_model():
-#     return model
-#-----------------
+@app.route('/', methods=['POST', 'GET'])
+def doit():
+    mess1 = ''
+    mess2 = ''
+    if request.method == 'POST':
+        Xst = []    
+        Xst.append(request.form.get("feature1"))
+        Xst.append(request.form.get("feature2"))
+        Xst.append(request.form.get("feature3"))
+        Xst.append(request.form.get("feature4"))
+        Xst.append(request.form.get("feature5"))
+        Xst.append(request.form.get("feature6"))
+        Xst.append(request.form.get("feature7"))
+        Xst.append(request.form.get("feature8"))
+        Xst.append(request.form.get("feature9"))
+        Xst.append(request.form.get("feature10"))
 
+        X = [float(i) for i in Xst if i.replace('.', '').isdigit()]
 
-def check_username(username, password):
-    if len(username) < 3:
-        message = "Incorrect username"
-    else:
-        message = f"User {username} has password {password}"
-    return message
+        if len(X) != 10:
+            mess1 = f"Неверное количество параметров {len(X)}, должно быть 10"
+        else:
+            X[5] = X[5] ** 0.5
+            X = np.array(X).reshape(1, -1)
 
+            model1 = load('Прочность при растяжении.joblib')
+            transformer = Normalizer()
+            xn = transformer.fit_transform(X)
 
-@app.route("/")
-def index():
-    return "Главная страница"
+            mess1 = f'Ожидаемая прочность при растяжении, МПа: {round(model1.predict(xn)[0], 3)}'
+            mess2 = str(xn)
 
-
-@app.route("/login/", methods=["post", "get"])
-def get_password():
-#     model = get_model()
-    message = "Test"
-    
-    if request.method == "POST":
-        u = request.form.get("username")
-        p = request.form.get("password")
-        
-        message = check_username(u, p)
-    
-    return render_template("login.html", message=message)
-
+    return render_template('index.html',
+                           message='Прогноз модуля упругости при растяжении, прочности при растяжении',
+                           message1=mess1,
+                           message2=mess2
+                           )
 
 app.run()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
